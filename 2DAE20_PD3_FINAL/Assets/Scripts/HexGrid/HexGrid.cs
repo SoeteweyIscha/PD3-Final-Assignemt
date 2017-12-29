d6b1944a986a64d9714666c5180d6906f21e8c2c
Ischa <Soeteweyischa@gmail.com>
2017-12-29 14:05:31 +0100

    Added Grid Generation from previous project version

---
+++ 2DAE20_PD3_FINAL/Assets/Scripts/HexGrid/HexGrid.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HexScript : MonoBehaviour
+{
+
+    [SerializeField]
+    private int _column;
+    [SerializeField]
+    private int _row;
+
+
+    public int Row
+    {
+        get { return _row; }
+        set { _row = value; }
+    }
+
+    public int Column
+    {
+        get { return _column; }
+        set { _column = value; }
+    }
+
+}
